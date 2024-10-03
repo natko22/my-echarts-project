5074d4b1fb739f09b4b291ed202fe221157a6a00
Natassa <112552395+natko22@users.noreply.github.com>
2024-10-03 19:18:14 +0200

    add icon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Charts Project",
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/apple-touch-icon.png",
+        }, // Favicon reference
+      ],
+    },
+  },
+});
